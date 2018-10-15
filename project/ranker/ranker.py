ac255ae92c188a75d8d235fdb0ea149343e81471
Daniel Matos <daniel.matos@pucp.pe>
2018-10-15 13:39:27 -0500

    Agrega retorno de scores por fold en metodo get_data de un ranker classifier

--- project/ranker/ranker.py
+++ project/ranker/ranker.py
@@ -3,6 +3,8 @@ from project.meta_feature.extractor import MFRCollection
 from project.feature_selection.feature_selector import FSRCollection
 from sklearn.base import BaseEstimator
 import numpy as np
+import pandas as pd
+
 
 class RankingPredictor(NearestNeighbors):
     def __init__(self, corpus_id, n_neighbors):
@@ -10,9 +12,27 @@ class RankingPredictor(NearestNeighbors):
         self.corpus_id = corpus_id
 
     def get_data(self):
-        mf_data = MFRCollection(self.corpus_id).load().toDataFrame()
-        fs_data = FSRCollection(self.corpus_id).load().ranking("max")
-        return mf_data, fs_data
+        df_mf = MFRCollection(self.corpus_id).load().toDataFrame()
+
+        fs_data = FSRCollection(self.corpus_id).load()
+        df_rank, df_scores = fs_data.ranking("max", return_scores=True)
+
+
+        fold_data =  []
+        for fsr in fs_data:
+            group = fsr.resultsToDataFrame().groupby(['pipeline',
+                                                      'fold']).max().T
+            pipelines = group.columns.levels[0]
+            fold_scores = {p: group[p].loc['score'] for p in pipelines}
+
+            fold_data.append({
+                'dataset': fsr.dataset_id,
+                **fold_scores,
+                })
+            
+        df_fold_scores = pd.DataFrame(fold_data).set_index('dataset')
+
+        return df_mf, df_rank, df_scores, df_fold_scores
 
     def fit(self, X, y):
         self._fit_y = y
