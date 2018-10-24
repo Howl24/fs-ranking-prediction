fbbdb6e03940832c6048e22e92f97515a731d25d
Daniel Matos <daniel.matos@pucp.pe>
2018-10-24 11:00:13 -0500

    Agrega rutas relativas de datasets y resultados

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
-META_FEATURE_RESULT_PATH = "/home/howl/Code/FS-Ranking/results/meta_feature/"
-FEATURE_SELECTION_RESULT_PATH = "/home/howl/Code/FS-Ranking/results/feature_selection/"
+META_FEATURE_RESULT_PATH = "./results/meta_feature/"
+FEATURE_SELECTION_RESULT_PATH = "./results/feature_selection/"
 
 CORPUS_PATH = {
-        "ma": "/home/howl/Code/FS-Ranking/datasets/MicroarrayData/",
-        "ma_test": "/home/howl/Code/FS-Ranking/datasets/Test/",
+        "ma": "./datasets/MicroarrayData/",
+        "ma_test": "./datasets/Test/",
         }
