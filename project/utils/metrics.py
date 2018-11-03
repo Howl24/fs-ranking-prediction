5eeb95d0319caddeeda2b3d21c9b7413ae48d55c
Daniel Matos <daniel.matos@pucp.pe>
2018-11-02 19:36:02 -0500

    Agrega metrica de promedio de acc loss

--- project/utils/metrics.py
+++ project/utils/metrics.py
@@ -10,9 +10,6 @@ import numpy as np
 import pandas as pd
 
 
-
-
-
 """
 METRICAS
 
@@ -24,13 +21,15 @@ Attributes:
     }
 """
 
+
 def spearman_rank_score(y_test, y_pred):
     coefs = []
     for yt, yp in zip(y_test, y_pred):
         coef, pval = spearmanr(yt, yp)
         coefs.append(coef)
-    
+
     return np.mean(coefs)
+
 
 def _accuracy_loss(y_score, y_pred):
     pred_scores = pd.DataFrame()
@@ -42,12 +41,13 @@ def _accuracy_loss(y_score, y_pred):
     max_scores = pred_scores.max(axis=1)
     loss_scores = abs(pred_scores.sub(max_scores, axis=0)).cummin(axis=1)
 
-    nfs = loss_scores.shape[1]
+    # nfs = loss_scores.shape[1]
     loss_auc = []
     for i in loss_scores.iterrows():
         loss_auc.append(auc(i[1].keys(), i[1].values))
 
     return np.mean(loss_auc)
+
 
 def accuracy_loss(y_scores, y_pred):
     results = []
@@ -55,6 +55,24 @@ def accuracy_loss(y_scores, y_pred):
         results.append(_accuracy_loss(ys, yp))
 
     return np.mean(results)
+
+
+def _mean_accuracy_loss(y_scores, y_pred, plot=False):
+    max_score = y_scores.max()
+    pred_scores = y_scores[np.argsort(y_pred)]
+    loss = max_score - np.maximum.accumulate(pred_scores)
+    x = np.linspace(1, loss.shape[0], loss.shape[0])
+
+    if plot:
+        plt.plot(x, loss)
+
+    return auc(x, loss)
+
+def mean_accuracy_loss(y_score, y_pred):
+    return np.mean([_mean_accuracy_loss(ys, yp)
+                   for ys, yp in zip(y_score, y_pred)])
+
+
 
 
 Metric = namedtuple("Metric", ["method", "kwargs", "name"])
@@ -72,11 +90,15 @@ METRICS = {
                            "Spearman-Rank-Coefficient"),
         'acc_loss': Metric(accuracy_loss, {},
                            "Max Accuracy Loss"),
+        'mean_acc_loss': Metric(mean_accuracy_loss, {},
+                           "Mean Accuracy Loss"),
         }
+
 
 def get_metric(metric_key):
     metric = METRICS[metric_key]
     return make_scorer(metric.method, **metric.kwargs)
+
 
 def evaluate_metric(metric_key, y_true, y_pred):
     metric = METRICS[metric_key]
