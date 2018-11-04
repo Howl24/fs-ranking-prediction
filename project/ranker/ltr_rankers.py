1baa04a83affb7711ce7394184f66bad2c7a63cf
Robert-Alonso <robert.alonso.ac@gmail.com>
2018-11-04 00:01:34 -0500

    Add Lambdarank results

--- project/ranker/ltr_rankers.py
+++ project/ranker/ltr_rankers.py
@@ -9,7 +9,8 @@ def wide2long(X, y):
     return np.concatenate([X_cont, X_cats], axis=1), y.reshape(-1)
 
 def cv_lgbm(lightgbm, X, y, y_scores, kfolds, params, num_rounds=1000, 
-            early_stopping_rounds=30, verbose_eval=False, metric='ndcg@13'):
+            early_stopping_rounds=30, verbose_eval=False, metric='ndcg@13', 
+            min_trees=1, verbose_folds=True):
     results = []
     models = []
     for idx, (trn_idx, val_idx) in enumerate(kfolds.split(X, y)):
@@ -31,29 +32,32 @@ def cv_lgbm(lightgbm, X, y, y_scores, kfolds, params, num_rounds=1000,
                              verbose_eval=verbose_eval)
         
         y_pred_trn = np.array([rankdata(bst.predict(wide2long(x[None,:],
-                                                              y[None,:])[0]), 
+                                                              y[None,:])[0],
+                               num_iteration=max(bst.best_iteration, min_trees)), 
                                method='ordinal') for x, y in zip(X_trn, y_trn)])
         y_pred_val = np.array([rankdata(bst.predict(wide2long(x[None,:], 
-                                                              y[None,:])[0]),
+                                                              y[None,:])[0],
+                               num_iteration=max(bst.best_iteration, min_trees)),
                                method='ordinal') for x, y in zip(X_val, y_val)])
 
         trn_spearman = evaluate_metric("spearman", y_trn, y_pred_trn)
-        trn_acc_loss = evaluate_metric("mean_acc_loss", y_scores_trn, 
-                                       y_trn.shape[1] - y_pred_trn + 1)
+        trn_acc_loss = evaluate_metric("mean_acc_loss", y_scores_trn, y_pred_trn)
         val_spearman = evaluate_metric("spearman", y_val, y_pred_val)
-        val_acc_loss = evaluate_metric("mean_acc_loss", y_scores_val, 
-                                       y_val.shape[1] - y_pred_val + 1)
+        val_acc_loss = evaluate_metric("mean_acc_loss", y_scores_val, y_pred_val)
         trn_ndcg = bst.best_score['training'][metric]
         val_ndcg = bst.best_score['valid_1'][metric]
         
-        print(f'Fold {idx + 1:>3} | '
-              f'#Est: {bst.best_iteration:>3} | '
-              f'Trn_Spearman: {trn_spearman: .4f} | '
-              f'Val_Spearman: {val_spearman: .4f} | '
-              f'Trn_ACCLoss: {trn_acc_loss: .4f} | '
-              f'Val_ACCLoss: {val_acc_loss: .4f} | '
-              f'Trn_NDCG: {trn_ndcg: .4f} | '
-              f'Val_NDCG: {val_ndcg: .4f}')
+        if verbose_folds:
+            num_iterations = max(bst.best_iteration, min_trees)
+            print(f'Fold {idx + 1:>3} | '
+                  f'#Est: {num_iterations:>3} | '
+                  f'Trn_Spearman: {trn_spearman: .4f} | '
+                  f'Val_Spearman: {val_spearman: .4f} | '
+                  f'Trn_ACCLoss: {trn_acc_loss: .4f} | '
+                  f'Val_ACCLoss: {val_acc_loss: .4f} | '
+                  f'Trn_NDCG: {trn_ndcg: .4f} | '
+                  f'Val_NDCG: {val_ndcg: .4f}')
+            
         results.append((trn_spearman, val_spearman, 
                         trn_acc_loss, val_acc_loss,
                         trn_ndcg, val_ndcg))
@@ -69,3 +73,37 @@ def cv_lgbm(lightgbm, X, y, y_scores, kfolds, params, num_rounds=1000,
           f'Val_NDCG:     {results[:,5].mean(): .4f} +/-{results[:,5].std():.4f}')
     print()
     return results, models
+
+def cv_random(rr, X, y, y_scores, kfolds):
+    results = []
+    for idx, (trn_idx, val_idx) in enumerate(kfolds.split(X, y)):
+        X_val, y_val = X[val_idx], y[val_idx]
+        X_trn, y_trn = X[trn_idx], y[trn_idx]
+        y_scores_trn = y_scores[trn_idx]
+        y_scores_val = y_scores[val_idx]
+        
+        rr.fit(X_trn, y_trn)
+        y_pred_trn = rr.predict(y_trn)
+        y_pred_val = rr.predict(y_val)
+        
+        trn_spearman = evaluate_metric("spearman", y_trn, y_pred_trn)
+        trn_acc_loss = evaluate_metric("mean_acc_loss", y_scores_trn, y_pred_trn)
+        val_spearman = evaluate_metric("spearman", y_val, y_pred_val)
+        val_acc_loss = evaluate_metric("mean_acc_loss", y_scores_val, y_pred_val)
+        
+        print(f'Fold {idx + 1:>3} | '
+              f'Trn_Spearman: {trn_spearman: .4f} | '
+              f'Val_Spearman: {val_spearman: .4f} | '
+              f'Trn_ACCLoss: {trn_acc_loss: .4f} | '
+              f'Val_ACCLoss: {val_acc_loss: .4f}')
+        results.append((trn_spearman, val_spearman, 
+                        trn_acc_loss, val_acc_loss))
+          
+    results = np.array(results)
+    print()
+    print(f'Trn_Spearman: {results[:,0].mean(): .4f} +/-{results[:,0].std():.4f} | '
+          f'Val_Spearman: {results[:,1].mean(): .4f} +/-{results[:,1].std():.4f}\n'
+          f'Trn_ACCLoss:  {results[:,2].mean(): .4f} +/-{results[:,2].std():.4f} | '
+          f'Val_ACCLoss:  {results[:,3].mean(): .4f} +/-{results[:,3].std():.4f}')
+    print()
+    return results
